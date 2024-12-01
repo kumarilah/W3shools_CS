8d9458755ff0e97ef54f326eeb636e84616ad900
kumarilah <gideonamoo48@gmail.com>
2024-12-01 22:05:35 +0000

    updated

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter your PIN");
+            Int passcode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your PIN is: " + passcode);
+
+        }
+    }
+}
